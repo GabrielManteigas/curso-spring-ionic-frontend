863b6d5d31c8823a25eabc2c20e832896c176e16
GabrielManteigas <40912@alunos.isel.ipl.pt>
2022-01-09 10:42:35 +0000

    Criado um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -38,10 +38,7 @@ export class CategoriasPage {
     this.categoriaService.findAll()
       .subscribe(response => {
         this.items = response;
-        console.log(response)
-      }, error => {
-        console.log(error)
-      });
+      }, error => {});
       
   }
 
